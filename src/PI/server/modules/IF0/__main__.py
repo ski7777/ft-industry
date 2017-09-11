3a1eab1172258b79733bf7da22c0604a1bf76241
Raphael Jacob <r.jacob2002@gmail.com>
2017-09-11 17:11:20 +0200

    Finish IF0 abstraction

--- src/PI/server/modules/IF0/__main__.py
+++ src/PI/server/modules/IF0/__main__.py
@@ -9,9 +9,13 @@ import time
 class Interface():
     def __init__(self):
         self.IF = RI.RoboInterface()
-        self.F1 = Belt(1, 1, self.IF)
+        self.F1 = F1(self.IF)
         self.F2 = F2(4, 5, self.IF)
         self.Sled = Sled(self.IF)
+        self.Stamp = Stamp(9, self.IF)
+
+    def getSupplyVoltage(self):
+        return(self.IF.av_to_v(self.IF.GetAV))
 
 
 class Sled():
@@ -23,6 +27,12 @@ class Sled():
         self.IF = IF
 
         self.isInit = False
+        self.curPosBelt = -1
+        self.curPosSled = -1
+
+        self.POSBELTHOME = 0
+        self.POSBELTHANDOVER = 1
+        self.POSSLEDHOME = 0
 
     def initialize(self, home=True):
         if not self.IF.Digital(self.ButEnd):
@@ -30,17 +40,17 @@ class Sled():
             while not self.IF.Digital(self.ButEnd):
                 pass
         self.IF.SetMotor(self.MotSled)
-        time.sleep(1)
+        time.sleep(2)
         self.IF.SetMotor(self.MotSled, 's')
 
         self.IF.SetMotor(self.MotBelt)
-        time.sleep(0.5)
         while True:
             if not self.IF.Digital(self.PhotoBelt):
                 time.sleep(0.01)
                 if not self.IF.Digital(self.PhotoBelt):
                     self.IF.SetMotor(self.MotBelt, 's')
                     break
+        self.curPosBelt = self.POSBELTHOME
         if home:
             self.home()
 
@@ -50,6 +60,27 @@ class Sled():
             while not self.IF.Digital(self.ButEnd):
                 pass
             self.IF.SetMotor(self.MotSled, 's')
+        self.curPosSled = self.POSSLEDHOME
+
+    def goPosBelt(self, pos):
+        if pos == self.POSBELTHOME:
+            if self.curPosBelt != self.POSBELTHOME:
+                self.IF.SetMotor(self.MotBelt)
+                time.sleep(0.5)
+                while True:
+                    if not self.IF.Digital(self.PhotoBelt):
+                        time.sleep(0.01)
+                        if not self.IF.Digital(self.PhotoBelt):
+                            self.IF.SetMotor(self.MotBelt, 's')
+                            break
+                self.curPosBelt = self.POSBELTHOME
+        elif pos == self.POSBELTHANDOVER:
+            if self.curPosBelt != self.POSBELTHANDOVER:
+                self.goPosBelt(self.POSBELTHOME)
+                self.IF.SetMotor(self.MotBelt)
+                time.sleep(0.8)
+                self.IF.SetMotor(self.MotBelt, 's')
+                self.curPosBelt = self.POSBELTHANDOVER
 
 
 class Belt():
@@ -59,6 +90,9 @@ class Belt():
         self.IF = IF
 
         self.isInit = False
+        self.curPosBelt = -1
+
+        self.POSBELTHOME = 0
 
     def initialize(self):
         self.IF.SetMotor(self.Mot)
@@ -69,11 +103,52 @@ class Belt():
                 if not self.IF.Digital(self.PhotoInit):
                     self.IF.SetMotor(self.Mot, 's')
                     break
+        self.curPos = self.POSBELTHOME
 
 # This Belt can be adjusted
 # class demoBelt(Belt):
 #    def __init__(self, Mot, PhotoInit, IF):
 #        super().__init__(Mot, PhotoInit, IF)
+
+
+class Stamp():
+    def __init__(self, Valve, IF):
+        self.Valve = Valve
+        self.IF = IF
+
+        self.curPosStamp = 0
+
+        self.POSSTAMPUP = 0
+        self.POSSTAMPSOWN = 1
+
+    def goPos(self, pos):
+        if pos == self.POSSTAMPUP:
+            self.IF.SetOutput(self.Valve, 0)
+        elif pos == self.POSSTAMPSOWN:
+            self.IF.SetOutput(self.Valve, 7)
+
+
+class F1(Belt):
+    def __init__(self, IF):
+        super().__init__(1, 1, IF)
+        self.POSBELTTAMP = 1
+
+    def goPos(self, pos):
+        if pos == self.POSBELTHOME:
+            if self.curPosBelt != self.POSBELTHOME:
+                self.initialize()
+        elif pos == self.POSBELTTAMP:
+            self.goPos(self.POSBELTHOME)
+            self.IF.SetMotor(self.Mot, 'l', 3)
+            time.sleep(0.5)
+            while True:
+                if not self.IF.Digital(self.PhotoInit):
+                    time.sleep(0.01)
+                    if not self.IF.Digital(self.PhotoInit):
+                        time.sleep(0.1)
+                        self.IF.SetMotor(self.Mot, 's')
+                        break
+            self.curPosBelt = self.POSBELTTAMP
 
 
 class F2(Belt):
